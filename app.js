433b1814279e5033d9624ac009e19c54140f0182
Glyne J. Gittens <Jgit121@gmail.com>
2016-09-28 21:26:46 -0400

    Wrote http post request using node.

---
+++ app.js
@@ -0,0 +1,40 @@
+const http  = require ('http');
+const https = require ('https');
+const fs    = require ('fs');
+
+const data2040  = {
+  "token": "0217347fbdb52f16ea562bd939c1620a",
+  "github": "https://github.com/ArtBears/code2040app"
+}
+
+const options = {
+  hostname: 'challenge.code2040.org',
+  port: '80',
+  path: '/api/register',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(data2040)
+  }
+}
+
+let req = http.request(options, (res) => {
+  console.log("Status: " + res.statusCode);
+  console.log("Headers: " + JSON.stringify(res.headers));
+  res.setEncoding('utf-8');
+  
+  res.on('data', (chunk) => {
+    console.log("Response: " + chunk);
+  });
+  
+  res.on('end', () => {
+    console.log('All Done!');
+  });
+
+  res.on('error', (e) => {
+    console.log("An error occured: " + e.message )
+  });
+});
+
+req.write(data2040);
+req.end();
