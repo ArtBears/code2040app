d00d3774bd5e09d7dca2a27f0761cbb6e35e8f08
Glyne J. Gittens <Jgit121@gmail.com>
2016-10-09 14:29:55 -0400

    Created Index pages

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+let endpoint,
+    data_to_send,
+    request;
+
 // The current projects Endpoint
-let endpoint =  "/api/register"
+endpoint =  "/api/register"
 
 // The data to send in the current request
-let data_to_send = {
+data_to_send = {
     "token": "0217347fbdb52f16ea562bd939c1620a",
     "github": "https://github.com/ArtBears/code2040app"
 }
 
 // The post request that I will send
-let request = (endpoint, payload) => { 
+request = (endpoint, payload) => { 
   xhr = new XMLHttpRequest();
   let url = "http://challenge.code2040.org" + endpoint;
   xhr.open("POST", url, true);
--- step5/script.js
+++ step5/script.js
@@ -1,63 +1,51 @@
-// The current projects Endpoint
-let endpoint =  "/api/dating"
-let validate_endpoint = "/api/dating/validate"
 
-// The data to send in the current request
-let token_to_send = {
+let endpoint,
+    validate_endpoint,
+    token_to_send,
+    abstract_api_request,
+    promise,
+    find_date;
+
+// The current projects Endpoint
+endpoint =  "/api/dating"
+validate_endpoint = "/api/dating/validate"
+token_to_send = {
     "token": "0217347fbdb52f16ea562bd939c1620a"
 }
 
-// The post response with the reversed string
-let string_response = (endpoint, payload) => { 
-  xhr = new XMLHttpRequest();
+
+abstract_api_request = (endpoint) => {
+  let xhr = new XMLHttpRequest();
   let url = "http://challenge.code2040.org" + endpoint;
   xhr.open("POST", url, true);
   xhr.setRequestHeader("Content-type", "application/json");
-  xhr.onreadystatechange = function () { 
-      if (xhr.readyState == 4 && xhr.status == 200) {
-          console.log("Response: " + xhr.responseText);
-          console.log("sent");
-          console.log(xhr.responseText);
-
-      }
-  }
-  let data = JSON.stringify(payload);
-  xhr.send(data);
+  return xhr;
 }
 
-// The post request that I will send as a part of a promise
+// The POST request that I will send as a part of a promise
 
-let promise = new Promise((resolve, reject) => {
-  xhr = new XMLHttpRequest();
-  let url = "http://challenge.code2040.org" + endpoint;
-  xhr.open("POST", url, true);
-  xhr.setRequestHeader("Content-type", "application/json");
-  xhr.onreadystatechange = function () { 
-      if (xhr.readyState == 4 && xhr.status == 200) {
-          console.log("Response: " + xhr.responseText);
+promise = new Promise((resolve, reject) => {
+  let xhr_request = abstract_api_request(endpoint);
+  xhr_request.onreadystatechange = function () { 
+      if (xhr_request.readyState == 4 && xhr_request.status == 200) {
+          console.log("Response: " + xhr_request.responseText);
           console.log("sent");
-          resolve(xhr.responseText);
-
+          resolve(xhr_request.responseText);
       }
   }
   let data = JSON.stringify(token_to_send);
-  xhr.send(data);
+  xhr_request.send(data);
 });
 
 // iterate over dictionary
-let find_date = (datestamp, interval) => {
+find_date = (datestamp, interval) => {
   // turn the interval into milliseconds
   interval_in_milliseconds = Number.parseFloat(interval) * 1000;
-  console.log(interval_in_milliseconds);
   let date = new Date(datestamp);
-  console.log(date.getTime());
-  console.log(date);
   let date_in_milliseconds = Date.parse(date);
-  console.log(date_in_milliseconds);
   let combined_date = new Date(date_in_milliseconds + interval_in_milliseconds);
   let combined_date_string = combined_date.toISOString();
   combined_date_string = combined_date_string.replace(".000", "");
-  console.log(combined_date_string);
   return combined_date_string;
 }
 
@@ -70,8 +58,16 @@ promise.then((val) => {
     token: "0217347fbdb52f16ea562bd939c1620a",
     datestamp: new_date
   }
-  string_response(validate_endpoint, data_to_send);
-
+  let xhr_response = abstract_api_request(validate_endpoint);
+  xhr_response.onreadystatechange = function () { 
+    if (xhr_response.readyState == 4 && xhr_response.status == 200) {
+        console.log("Response: " + xhr_response.responseText);
+        console.log("sent");
+        console.log(xhr_response.responseText);
+    }
+  }
+  let data = JSON.stringify(data_to_send);
+  xhr_response.send(data);
   console.log(val);
 });
 
