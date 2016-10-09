eeae4b326dabf9ac2f441c93841c7b9d6985f764
Glyne J. Gittens <Jgit121@gmail.com>
2016-10-08 20:33:40 -0400

    Finished Challenge

--- step2/script.js
+++ step2/script.js
@@ -32,7 +32,7 @@ let reverse = (str) => {
 }
 
 // The post response with the reversed string
-let string_response = (endpoint, payload,) => { 
+let string_response = (endpoint, payload) => { 
   xhr = new XMLHttpRequest();
   let url = "http://challenge.code2040.org" + endpoint;
   xhr.open("POST", url, true);
@@ -49,8 +49,9 @@ let string_response = (endpoint, payload,) => {
   xhr.send(data);
 }
 
-// The post request that I will send
-let string_request = (endpoint, payload) => { 
+// The post request that I will send as a part of a promise
+
+let promise = new Promise((resolve, reject) => {
   xhr = new XMLHttpRequest();
   let url = "http://challenge.code2040.org" + endpoint;
   xhr.open("POST", url, true);
@@ -59,31 +60,21 @@ let string_request = (endpoint, payload) => {
       if (xhr.readyState == 4 && xhr.status == 200) {
           console.log("Response: " + xhr.responseText);
           console.log("sent");
-          return xhr.responseText;
+          resolve(xhr.responseText);
 
       }
   }
-  let data = JSON.stringify(payload);
+  let data = JSON.stringify(token_to_send);
   xhr.send(data);
-}
-
-let promise = new Promise((resolve, reject) => {
-  
 });
 
+promise.then((val) => {
+  let data_to_send = {
+  "token": "0217347fbdb52f16ea562bd939c1620a",
+  "string": reverse(val)
+  }
+  string_response(validate_endpoint, data_to_send)
 
-let api_response = string_request(endpoint, token_to_send);
-// let reversed_api_response = reverse(api_response);
-console.log(typeof(api_response));
-// let data_to_send = {
-//   "token": "0217347fbdb52f16ea562bd939c1620a",
-//   "string": reversed_api_response
-// }
+  console.log(val);
+});
 
-// string_response(validate_endpoint, data_to_send);
-
-
-
-
-
-console.log(reverse("paste"));
